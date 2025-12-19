749cc9472bd0c95519928c42115e7aeff6e648b0
Joseph Havens <joe.havens79@ku.edu>
2025-12-19 12:02:20 -0600

    add new page

--- assets/config.js
+++ assets/config.js
@@ -3,17 +3,22 @@ let config = {
   "Name": "Joseph Havens",
   "Description": "Undergraduate Researcher | Dr. Allison Kirkpatrick | KU Physics and Astronomy Dept.",
   "links": [
-  {
+    {
       "Title": "My Research Projects",
       "URL": "research.html",
       "icon_classes": "fa-solid fa-flask",
+    },
+    {
+      "Title": "Class Projects & Coursework",
+      "URL": "projects.html", // We will create this file next
+      "icon_classes": "fa-solid fa-laptop-code",
     },
     {
       "Title": "View My CV",
       "URL": "assets/CV_Havens.pdf",
       "icon_classes": "fas fa-file-alt",
     },
-{
+    {
       "Title": "Talks & Outreach",
       "URL": "outreach.html",
       "icon_classes": "fas fa-star",
