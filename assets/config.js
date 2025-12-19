34cc5a17f0fe9bda8a1fc5d6f8f3dbd4e8a2d658
Joseph Havens <joe.havens79@ku.edu>
2025-12-19 16:53:19 -0600

    Refactor config.js with updated links and icons
    Updated the config object to include new links and modified existing ones.

--- assets/config.js
+++ assets/config.js
@@ -1,37 +1,41 @@
-let config = {
-  "Title": "Joseph Havens",
-  "Name": "Joseph Havens",
-  "Description": "Undergraduate Researcher | Dr. Allison Kirkpatrick | KU Physics and Astronomy Dept.",
-  "links": [
-    {
-      "Title": "My Research Projects",
-      "URL": "research.html",
-      "icon_classes": "fa-solid fa-flask",
-    },
-    {
-      "Title": "Class Projects & Papers",
-      "URL": "projects.html", // We will create this file next
-      "icon_classes": "fa-solid fa-laptop-code",
-    },
-    {
-      "Title": "View My CV",
-      "URL": "assets/CV_Havens.pdf",
-      "icon_classes": "fas fa-file-alt",
-    },
-    {
-      "Title": "Talks & Outreach",
-      "URL": "outreach.html",
-      "icon_classes": "fas fa-star",
-    },
-    {
-      "Title": "LinkedIn",
-      "URL": "https://www.linkedin.com/in/joseph-havens-1b3a73348/",
-      "icon_classes": "fab fa-linkedin"
-    },
-    {
-      "Title": "Bluesky",
-      "URL": "https://bsky.app/profile/cosmicjoe.bsky.social",
-      "icon_classes": "fa-brands fa-bluesky"
-    },
-  ]
-}
+const config = {
+    "links": [
+        {
+            "Title": "My Research Projects",
+            "URL": "research.html",
+            "icon_classes": "fas fa-galaxy", // or fa-star
+        },
+        {
+            "Title": "Class Projects & Papers",
+            "URL": "projects.html",
+            "icon_classes": "fas fa-microchip",
+        },
+        {
+            "Title": "View My CV",
+            "URL": "assets/pdf/Joseph_Havens_CV.pdf",
+            "icon_classes": "fas fa-file-pdf",
+        },
+        // --- NEW BUTTON ---
+        {
+            "Title": "About Me",
+            "URL": "about.html",
+            "icon_classes": "fas fa-user-astronaut",
+        },
+        // ------------------
+        {
+            "Title": "Talks & Outreach",
+            "URL": "outreach.html",
+            "icon_classes": "fas fa-person-chalkboard",
+        },
+        {
+            "Title": "LinkedIn",
+            "URL": "https://www.linkedin.com/in/joseph-havens-1b3a73348/",
+            "icon_classes": "fab fa-linkedin",
+        },
+        {
+            "Title": "Bluesky",
+            "URL": "https://bsky.app/profile/cosmicjoe.bsky.social",
+            "icon_classes": "fa-brands fa-bluesky",
+        }
+    ]
+};
